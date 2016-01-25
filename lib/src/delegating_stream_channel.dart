3ea2d8c21c12ad72e98c5b2e0050913201fd6282
Natalie Weizenbaum <nweiz@google.com>
2016-01-25 15:09:57 -0800

    Fix DelegatingStreamChannel.
    I apparently forgot to add imports and named it the wrong thing.
    
    R=rnystrom@google.com
    
    Review URL: https://codereview.chromium.org//1615043004 .

--- lib/src/delegating_stream_channel.dart
+++ lib/src/delegating_stream_channel.dart
@@ -2,16 +2,20 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
+
+import '../stream_channel.dart';
+
 /// A simple delegating wrapper around [StreamChannel].
 ///
 /// Subclasses can override individual methods, or use this to expose only
 /// [StreamChannel] methods.
-class StreamChannelView<T> extends StreamChannelMixin<T> {
+class DelegatingStreamChannel<T> extends StreamChannelMixin<T> {
   /// The inner channel to which methods are forwarded.
   final StreamChannel<T> _inner;
 
   Stream<T> get stream => _inner.stream;
   StreamSink<T> get sink => _inner.sink;
 
-  StreamChannelView(this._inner);
+  DelegatingStreamChannel(this._inner);
 }
